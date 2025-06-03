79368cad5e032c0f9e7d3b7379ca2c68f2b8dd9b
Bertodo Neto <bertodoaluno10@gmail.com>
2025-06-02 22:30:49 -0400

    auth controller, module e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { DisciplinasModule } from './disciplinas/disciplinas.module';
 import { MatriculasModule } from './matriculas/matriculas.module';
 import { JogosModule } from './jogos/jogos.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}), UsuariosModule, ProfessoresModule, EstudantesModule, DisciplinasModule, MatriculasModule, JogosModule],
+  imports: [ConfigModule.forRoot({isGlobal:true}), UsuariosModule, ProfessoresModule, EstudantesModule, DisciplinasModule, MatriculasModule, JogosModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
