535a596c29b566d4fd79e74ab001bc134ed6672d
Bertodo Neto <bertodoaluno10@gmail.com>
2025-06-03 22:21:31 -0400

    atividade 33

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,8 +5,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { JwtStrategy } from './jwt.strategy';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 @Module({
   imports: [
     UsuariosModule,
