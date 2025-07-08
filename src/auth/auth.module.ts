c6e7ee07c0e139c2a8784dbd05143516cf5228b5
Bertodo Neto <bertodoaluno10@gmail.com>
2025-07-07 20:12:49 -0400

    teste de banco

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { JwtStrategy } from './jwt.strategy';
+
 @Module({
   imports: [
     UsuariosModule,
@@ -19,24 +20,5 @@ import { JwtStrategy } from './jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-})
-export class AuthModule { }
-
-
-
-
-
-
-
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsuariosService } from '../usuarios/usuarios.service';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService, UsuariosService, PrismaService],
 })
 export class AuthModule {}
